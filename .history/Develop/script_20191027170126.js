de86adb2fd54e6ad9398b132a596948095a326c7
Sophie <sophie88leigh@gmail.com>
2019-10-27 15:07:39 -0400

    used moments to set work hours, set/getItem to/from local storage, set past, present, future colors.

---
+++ .history/Develop/script_20191027170126.js
@@ -0,0 +1,51 @@
+var startTime = 9;
+var endTime = 17
+timeSlots = $(".container");
+timeSlots = document.querySelector(".container")
+
+// for(i = startTime; i < endTime; i++) {
+    newHour = $("<div>");
+    newHour.addClass("row");
+
+    hour = $("<div>");
+    hour.addClass("col-2 hour");
+    hour.text(i);
+
+    dailyTask = $("<textarea>");
+    dailyTask.addClass("col-8");
+    dailyTask.addClass("future");
+   
+    saveButton = $("<button>");
+    saveButton.addClass("col-2 saveBtn");
+    saveButton.addClass("saveBtn i:hover");
+    saveButton.text("save");
+    
+    newHour.append(hour, dailyTask, saveButton);
+    timeSlots.append(newHour);
+//  }
+ 
+
+// var saveButton = $(".saveBtn");
+
+//  var inputTask = $(".col-8").value;
+
+// $(".saveBtn").on("click", function(){
+ 
+//   var input = inputTask;
+//   input.textContent=value;
+
+//  if (input && value) {
+//    localStorage.setItem("input", input);
+//  }
+  // console.log("clicked save button");
+    // });
+
+    // // Get id of element clicked by user
+    // $(".row").click(function(event) {
+    //   dailyTaskInput = event.target.class;
+    
+    // }
+
+
+    // var saveButton=JSON.stringify(value);
+    // localStorage.setItem("saveButton", saveButtonJSON);
